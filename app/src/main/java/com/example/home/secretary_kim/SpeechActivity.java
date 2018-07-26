deddf5310fc734027f30699018fd00f0be7c8d85
ShimYoungwoo <youngwoo0411@naver.com>
2018-07-26 22:28:07 +0900

    Add files via upload
    음성인식

---
+++ app/src/main/java/com/example/home/secretary_kim/SpeechActivity.java
@@ -0,0 +1,226 @@
+package com.example.s0woo.tempvoice;
+//package com.example.home.secretary_kim;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.kakao.sdk.newtoneapi.SpeechRecognizeListener;
+import com.kakao.sdk.newtoneapi.SpeechRecognizerClient;
+import com.kakao.sdk.newtoneapi.SpeechRecognizerManager;
+import com.kakao.sdk.newtoneapi.impl.util.PermissionUtils;
+
+import java.util.ArrayList;
+
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+/**
+ * Created by s0woo on 2018-07-15.
+ */
+
+public class SpeechActivity extends Activity implements View.OnClickListener, SpeechRecognizeListener{
+    private SpeechRecognizerClient client;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speech);
+
+
+        //나중에 permission 분리할 수 있다면 하는걸로.. 처음 시작할 때 넣는게 나을 거 같다
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)
+                    && ActivityCompat.shouldShowRequestPermissionRationale(this, WRITE_EXTERNAL_STORAGE)) {
+                ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            } else {
+
+            }
+        } else {
+            //startUsingSpeechSDK();
+        }
+
+
+        SpeechRecognizerManager.getInstance().initializeLibrary(this);
+        findViewById(R.id.speechbutton).setOnClickListener(this);
+        findViewById(R.id.cancelbutton).setOnClickListener(this);
+        findViewById(R.id.restartbutton).setOnClickListener(this);
+        findViewById(R.id.stopbutton).setOnClickListener(this);
+        setButtonsStatus(true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    //startUsingSpeechSDK();
+                } else {
+                    finish();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        // API를 더이상 사용하지 않을 때 finalizeLibrary()를 호출한다.
+        SpeechRecognizerManager.getInstance().finalizeLibrary();
+    }
+
+    private void setButtonsStatus(boolean enabled) {
+        findViewById(R.id.speechbutton).setEnabled(enabled);
+        findViewById(R.id.restartbutton).setEnabled(!enabled);
+        findViewById(R.id.cancelbutton).setEnabled(!enabled);
+        findViewById(R.id.stopbutton).setEnabled(!enabled);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        String serviceType = SpeechRecognizerClient.SERVICE_TYPE_WEB;
+
+        // 음성인식 버튼 listener
+        if (id == R.id.speechbutton) {
+            if(PermissionUtils.checkAudioRecordPermission(this)) {
+
+                SpeechRecognizerClient.Builder builder = new SpeechRecognizerClient.Builder().
+                        setServiceType(serviceType);
+
+                if (serviceType.equals(SpeechRecognizerClient.SERVICE_TYPE_WORD)) {
+                    EditText words = (EditText)findViewById(R.id.words_edit);
+                    String wordList = words.getText().toString();
+                    builder.setUserDictionary(wordList);
+
+                    Log.i("SpeechSampleActivity", "word list : " + wordList.replace('\n', ','));
+                }
+
+                client = builder.build();
+
+                client.setSpeechRecognizeListener(this);
+                client.startRecording(true);
+
+                setButtonsStatus(false);
+            }
+        }
+        // 음성인식 취소버튼 listener
+        else if (id == R.id.cancelbutton) {
+            if (client != null) {
+                client.cancelRecording();
+            }
+
+            setButtonsStatus(true);
+        }
+        // 음성인식 재시작버튼 listener
+        else if (id == R.id.restartbutton) {
+            if (client != null) {
+                client.cancelRecording();
+                client.startRecording(true);
+            }
+        }
+        // 음성인식 중지버튼 listener
+        else if (id == R.id.stopbutton) {
+            if (client != null) {
+                client.stopRecording();
+            }
+        }
+    }
+
+    @Override
+    public void onReady() {
+
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+
+    }
+
+    @Override
+    public void onError(int errorCode, String errorMsg) {
+        Log.e("SpeechSampleActivity", "onError");
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                setButtonsStatus(true);
+            }
+        });
+
+        client = null;
+    }
+
+    @Override
+    public void onPartialResult(String partialResult) {
+
+    }
+
+    @Override
+    public void onResults(Bundle results) {
+        final StringBuilder builder = new StringBuilder();
+        Log.i("SpeechSampleActivity", "onResults");
+
+        ArrayList<String> texts = results.getStringArrayList(SpeechRecognizerClient.KEY_RECOGNITION_RESULTS);
+        ArrayList<Integer> confs = results.getIntegerArrayList(SpeechRecognizerClient.KEY_CONFIDENCE_VALUES);
+
+        for (int i = 0; i < texts.size(); i++) {
+            builder.append(texts.get(i));
+            builder.append(" (");
+            builder.append(confs.get(i).intValue());
+            builder.append(")\n");
+        }
+
+        final Activity activity = this;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // finishing일때는 처리하지 않는다.
+                if (activity.isFinishing()) return;
+
+                AlertDialog.Builder dialog = new AlertDialog.Builder(activity).
+                        setMessage(builder.toString()).
+                        setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                dialog.show();
+
+                setButtonsStatus(true);
+            }
+        });
+
+        client = null;
+    }
+
+    @Override
+    public void onAudioLevel(float audioLevel) {
+
+    }
+
+    @Override
+    public void onFinished() {
+        Log.i("SpeechSampleActivity", "onFinished");
+    }
+}
