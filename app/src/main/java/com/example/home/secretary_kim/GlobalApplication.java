1859b5042023ecc802ef30785fd3689803a9cbda
201524508 <a01045719803@gmail.com>
2018-07-31 15:33:03 +0900

    kakao login

---
+++ app/src/main/java/com/example/home/secretary_kim/GlobalApplication.java
@@ -0,0 +1,34 @@
+package com.example.home.secretary_kim;
+
+import android.app.Activity;
+import android.app.Application;
+import android.util.Log;
+
+import com.kakao.auth.KakaoSDK;
+
+public class GlobalApplication extends Application {
+    private static GlobalApplication mInstance;
+    private static volatile Activity currentActivity = null;
+
+    public static Activity getCurrentActivity() {
+        Log.d("TAG", "++ currentActivity : " + (currentActivity != null ? currentActivity.getClass().getSimpleName() : ""));
+        return currentActivity;
+    }
+
+    public static void setCurrentActivity(Activity currentActivity) {
+        GlobalApplication.currentActivity = currentActivity;
+    }
+
+    public static GlobalApplication getGlobalApplicationContext() {
+        if(mInstance == null)
+            throw new IllegalStateException("this application does not inherit GlobalApplication");
+        return mInstance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+        KakaoSDK.init(new KakaoSDKAdapter());
+    }
+}
--- app/src/main/java/com/example/home/secretary_kim/LoginActivity.java
+++ app/src/main/java/com/example/home/secretary_kim/LoginActivity.java
@@ -2,17 +2,33 @@ package com.example.home.secretary_kim;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.kakao.auth.AuthType;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.LoginButton;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.exception.KakaoException;
 
 import com.nhn.android.naverlogin.OAuthLogin;
 import com.nhn.android.naverlogin.OAuthLoginHandler;
 import com.nhn.android.naverlogin.ui.view.OAuthLoginButton;
+
+import java.security.MessageDigest;
 
 /**
  * Created by YUNA on 2018-07-13.
@@ -38,6 +54,17 @@ public class LoginActivity extends Activity {
 
     private OAuthLoginButton mOAuthLoginButton;
 
+    private SessionCallback mKakaocallback;
+    // view
+    //private Button login_button;
+    private LoginButton loginButton;
+    private TextView tv_user_id;
+    private TextView tv_user_name;
+    private ImageView iv_user_profile;
+    private String userName;
+    private String userId;
+    private String profileUrl;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +74,24 @@ public class LoginActivity extends Activity {
 
         initData();
         initView();
+
+        /*카카오 로그인*/
+        // 헤쉬키를 가져온다
+        getAppKeyHash();
+
+        /*
+        tv_user_id = (TextView) findViewById(R.id.tv_user_id);
+        tv_user_name = (TextView) findViewById(R.id.tv_user_name);
+        iv_user_profile = (ImageView) findViewById(R.id.iv_user_profile);
+        */
+        loginButton = (LoginButton) findViewById(R.id.btnKakaoLogin);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 카카오 로그인 요청
+                isKakaoLogin();
+            }
+        });
     }
 
     private void initData() {
@@ -197,4 +242,96 @@ public class LoginActivity extends Activity {
         }
     }
     */
+
+    /*카카오 로그인*/
+    private void isKakaoLogin() {
+        // 카카오 세션을 오픈한다
+        mKakaocallback = new SessionCallback();
+        com.kakao.auth.Session.getCurrentSession().addCallback(mKakaocallback);
+        com.kakao.auth.Session.getCurrentSession().checkAndImplicitOpen();
+        com.kakao.auth.Session.getCurrentSession().open(AuthType.KAKAO_TALK_EXCLUDE_NATIVE_LOGIN, LoginActivity.this);
+    }
+
+    protected void KakaorequestMe() {
+
+        UserManagement.getInstance().requestMe(new MeResponseCallback() {
+            @Override
+            public void onFailure(ErrorResult errorResult) {
+                int ErrorCode = errorResult.getErrorCode();
+                int ClientErrorCode = -777;
+
+                if (ErrorCode == ClientErrorCode) {
+                    Toast.makeText(getApplicationContext(), "카카오톡 서버의 네트워크가 불안정합니다. 잠시 후 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.d("TAG" , "오류로 카카오로그인 실패 ");
+                }
+            }
+
+            @Override
+            public void onSessionClosed(ErrorResult errorResult) {
+                Log.d("TAG" , "오류로 카카오로그인 실패 ");
+            }
+
+            @Override
+            public void onSuccess(UserProfile userProfile) {
+                profileUrl = userProfile.getProfileImagePath();
+                userId = String.valueOf(userProfile.getId());
+                userName = userProfile.getNickname();
+
+                /*
+                Log.i(TAG, "prifileUrl:" + profileUrl);
+                Log.i(TAG, "userId:" + userId);
+                Log.i(TAG, "userName:" + userName);
+*/
+                setLayoutText();
+            }
+
+            @Override
+            public void onNotSignedUp() {
+                // 자동가입이 아닐경우 동의창
+            }
+        });
+    }
+
+    private void setLayoutText(){
+        tv_user_id.setText(userId);
+        tv_user_name.setText(userName);
+
+        /*
+        Picasso.with(this)
+                .load(profileUrl)
+                .fit()
+                .into(iv_user_profile);*/
+    }
+
+    private void getAppKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                //Log.d(TAG, "getAppKeyHash > Hash key:" + something);
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            //og.e(TAG, "getAppKeyHash > name not found : " + e.toString());
+        }
+    }
+
+    private class SessionCallback implements ISessionCallback {
+        @Override
+        public void onSessionOpened() {
+            // 사용자 정보를 가져옴, 회원가입 미가입시 자동가입 시킴
+            KakaorequestMe();
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            if(exception != null) {
+                //Log.d(TAG, "Session CallBack Error > " + exception.getMessage());
+            }
+        }
+    }
 }
