9a8ce363370dfbb16d6da343caf16e9e8d74f586
ShimYoungwoo <youngwoo0411@naver.com>
2018-09-14 22:56:58 +0900

    Seperate RadioButton From LoginActivity

--- app/src/main/java/com/example/home/secretary_kim/LOGIN/LoginActivity.java
+++ app/src/main/java/com/example/home/secretary_kim/LOGIN/LoginActivity.java
@@ -1,24 +1,10 @@
 package com.example.home.secretary_kim.LOGIN;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.view.View;
-import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Toast;
 
-import com.example.home.secretary_kim.DBUserORSecretary;
-import com.example.home.secretary_kim.DBnotification;
-import com.example.home.secretary_kim.MyFirebaseMessagingService;
 import com.example.home.secretary_kim.R;
-import com.example.home.secretary_kim.VR.BluetoothActivity;
 import com.kakao.auth.ISessionCallback;
 import com.kakao.auth.Session;
 import com.kakao.util.exception.KakaoException;
@@ -30,38 +16,10 @@ public class LoginActivity extends Activity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_login);
 
         callback = new SessionCallback();
         Session.getCurrentSession().addCallback(callback);
-
-        final RadioGroup rg = (RadioGroup)findViewById(R.id.radioGroup1);
-        Button b = (Button)findViewById(R.id.button1);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int id = rg.getCheckedRadioButtonId();
-                RadioButton rb = (RadioButton)findViewById(id);
-                System.out.println("결과 : " + rb.getText().toString());
-
-                DBUserORSecretary SelectRole = new DBUserORSecretary(LoginActivity.this, "SelectRole.db", null, 1);
-                SQLiteDatabase dbRole;
-
-                dbRole = SelectRole.getWritableDatabase();
-                SelectRole.onCreate(dbRole);
-                ContentValues values = new ContentValues();
-
-                //values.put("num", 1);
-                values.put("role", rb.getText().toString());
-                //dbRole.insert("SelectRole", null,values);
-                dbRole.update("SelectRole", values, "1", null);
-                //dbRole.delete("SelectRole", "1", null);
-                String selectedRole = SelectRole.getResult();
-                System.out.println("db 입력값 : " + selectedRole);
-
-            }
-        });
 
     }
 
