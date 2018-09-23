37107def30e5db42db94ccbd2b5dfdf481522d00
yuna31 <siddl444@naver.com>
2018-09-23 12:39:13 +0900

    Update SelectRoleActivity.java

--- app/src/main/java/com/example/home/secretary_kim/LOGIN/SelectRoleActivity.java
+++ app/src/main/java/com/example/home/secretary_kim/LOGIN/SelectRoleActivity.java
@@ -7,6 +7,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -18,10 +20,35 @@ import com.example.home.secretary_kim.VR.BluetoothActivity;
 
 public class SelectRoleActivity extends Activity {
 
+    ImageView t1;
+    ImageButton btn_user;
+    ImageButton btn_manager;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selectrole);
 
+        t1 = (ImageView)findViewById(R.id.t1);
+        btn_user = (ImageButton)findViewById(R.id.btn_user);
+        btn_manager = (ImageButton)findViewById(R.id.btn_manager);
+
+        btn_user.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(SelectRoleActivity.this, BluetoothActivity.class);
+                startActivityForResult(i, 0);
+            }
+        });
+
+        btn_manager.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(SelectRoleActivity.this, SecretaryMenuActivity.class);
+                startActivityForResult(i, 0);
+            }
+        });
+
+/*
         final RadioGroup rg = (RadioGroup)findViewById(R.id.radioGroup1);
         Button b = (Button)findViewById(R.id.button1);
         b.setOnClickListener(new View.OnClickListener() {
@@ -48,15 +75,13 @@ public class SelectRoleActivity extends Activity {
                 System.out.println("db 입력값 : " + selectedRole);
 
                 if(selectedRole.equals("사용자")) {
-                    Intent i= new Intent(SelectRoleActivity.this, BluetoothActivity.class);
-                    startActivityForResult(i, 0);
+
                 } else if(selectedRole.equals("관리자")) {
-                    Intent i= new Intent(SelectRoleActivity.this, SecretaryMenuActivity.class);
-                    startActivityForResult(i, 0);
+
                 }
 
             }
-        });
+        });*/
     }
 
 }
