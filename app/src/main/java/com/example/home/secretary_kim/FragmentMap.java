dfc9b2eb92c2acc169340fa1265fc5e3811d04b3
yuna31 <siddl444@naver.com>
2018-09-25 19:37:39 +0900

    Update FragmentMap.java

--- app/src/main/java/com/example/home/secretary_kim/FragmentMap.java
+++ app/src/main/java/com/example/home/secretary_kim/FragmentMap.java
@@ -43,7 +43,7 @@ public class FragmentMap  extends BottomSheetDialogFragment {
         mBehavior = BottomSheetBehavior.from((View) view.getParent());
 
         adapter = new PointAdapter();
-        RecyclerView recyclerView = view.findViewById(R.id.base_recycler_view);
+        recyclerView = view.findViewById(R.id.base_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
         recyclerView.addItemDecoration(new DividerItemDecoration(view.getContext(), DividerItemDecoration.VERTICAL));
         recyclerView.setAdapter(adapter);
