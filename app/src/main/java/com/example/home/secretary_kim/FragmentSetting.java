f5be673a308414898549afa4a9454637ca91c79e
ShimYoungwoo <youngwoo0411@naver.com>
2018-09-30 22:49:25 +0900

    Update FragmentSetting.java

--- app/src/main/java/com/example/home/secretary_kim/FragmentSetting.java
+++ app/src/main/java/com/example/home/secretary_kim/FragmentSetting.java
@@ -17,9 +17,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import static com.example.home.secretary_kim.FragmentSettingActivity.*;
-
-
 public class FragmentSetting extends PreferenceFragmentCompat {
 
     SharedPreferences prefs;
